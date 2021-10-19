04e093e791951885a6c2779070a770a96f6cc94e
EvgeniiRomanov <e78rus@mail.ru>
2021-10-19 23:22:35 +0300

    Решены Лабораторные дома

--- Занятие2/Лабораторные_задания/task2_2/main.py
+++ Занятие2/Лабораторные_задания/task2_2/main.py
@@ -1,7 +1,9 @@
 if __name__ == "__main__":
-    sum_ = ...  # TODO заводим переменную для суммы
+    sum_ = list(range(0,10))
+    a = 0
+    #print(sum(sum_))
 
-    for number in ...:  # TODO диапазон чисел
-        ...
+    for number in sum_:
+        a += sum_[number]
 
-    # TODO печатаем ответ
+    print(a)
--- Занятие2/Лабораторные_задания/task2_4/main.py
+++ Занятие2/Лабораторные_задания/task2_4/main.py
@@ -1,4 +1,11 @@
 if __name__ == "__main__":
     # постарайтесь не использовать "магические" числа,
     # а по возможности дать переменным осмысленные названия и использовать их
-    # TODO Распечатать строку лесенкой
+
+    # slovo_ = list(input("Введите строку: "))
+    slovo_ = list("Hello,world")
+    # print(slovo_)
+    prob_number = 5
+    for value in range(len(slovo_)):
+        print(" " * prob_number, slovo_[value])
+        prob_number += 1
