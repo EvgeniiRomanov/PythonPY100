8b1609b8bd01d36d3fd6aa26b6098f546a17e167
EvgeniiRomanov <e78rus@mail.ru>
2021-10-19 13:24:04 +0300

    Сделал Практическое задание на работе

--- Занятие2/Практические_задания/task3_5/main.py
+++ Занятие2/Практические_задания/task3_5/main.py
@@ -1,3 +1,13 @@
-wind = int(input())
+wind = int(input("Введите скорость ветра: "))
 
+if wind in range(1, 5):
+    print("слабый")
+elif wind in range(5, 11):
+    print("умеренный")
+elif wind in range(12, 19):
+    print("сильный")
+elif wind > 19:
+    print("ураганный")
+else:
+    print("Такого ветра быть не может!")
 # TODO напишите с помощью if-elif-else условие проверки скорости ветра
