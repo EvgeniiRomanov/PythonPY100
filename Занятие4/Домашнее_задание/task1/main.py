2e088c58a24e904f3660aca5d788f79441947a53
EvgeniiRomanov <e78rus@mail.ru>
2021-11-05 14:19:01 +0300

    Подправил вариант решения ПЗ 4, ДЗ task1, не учли если число отрицательное? Нужно добавить abs!!!!!

--- Занятие4/Домашнее_задание/task1/main.py
+++ Занятие4/Домашнее_задание/task1/main.py
@@ -2,15 +2,20 @@ def polin_number(a1):
 
     #a = -66
     #num_a = [int(x) for x in str(a)]
+# -------------------------------1 способ--------------------------------
+    #num_a = str(abs(a1))
 
-    num_a = str(abs(a1))
+    # for i in range(len(num_a)-1):
+    #     if num_a[i] != num_a[i+1]:
+    #         return print(f"Цифры разные")
+    #
+    # return print(f"Цифры одинаковые")
+# ------------------ 2 способ ------------------------------------------------
+    n_list = [int(x) for x in str(abs(a1))]
+    print("Одинаковые") if len(set(n_list)) == 1 else print("Не одинаковые")
 
-    for i in range(len(num_a)-1):
-        if num_a[i] != num_a[i+1]:
-            return print(f"Цифры разные")
-
-    return print(f"Цифры одинаковые")
-
+    return
+# ------------------------------------------------------------------------
     # for i in num_a:
     #     if num_a.count(i) != len(num_a):
     #         return print(f"Цифры разные ")
